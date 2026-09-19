af599fcd7ed8452c24e0f796e9dced0cc4491ea7
iftekhar-shaikh <132425317+iftekhar-shaikh@users.noreply.github.com>
2026-09-19 11:53:09 +0500

    Wire Class 4 Urdu Qomi tehwar aur watan

--- ai4kids_school.py
+++ ai4kids_school.py
@@ -1117,6 +1117,7 @@ def render_topic_interactive(subject_key, grade, topic_title):
     (4, "english", "Formal letter"): "interactives/g4-formal-letter.html",
     (4, "urdu", "Hamd aur Naat"): "interactives/g4-hamd-naat.html",
     (4, "urdu", "Rahmat-e-Aalam aur seerat stories"): "interactives/g4-rahmat-seerat.html",
+    (4, "urdu", "Qomi tehwar aur watan"): "interactives/g4-qomi-tehwar.html",
     (4, "math", "Numbers 99,999 tak"): "interactives/g4-numbers-99999.html",
     (4, "math", "Number operations"): "interactives/g4-number-operations.html",
     (4, "math", "Factors aur multiples"): "interactives/g4-factors-multiples.html",
